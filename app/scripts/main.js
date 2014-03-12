5d61f00064229c98a526e24f6d89d967f4f37930
zaccary price <zaccary.price@avansera.com>
2014-03-12 10:37:00 +0100

    navigation menu. Closes #1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+
+$('#nav-link').click(function(e) {
+	e.preventDefault();
+	$('#header-navigation').toggleClass('open');
+	$('#shade').toggleClass('hide');
+});
